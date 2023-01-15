21909414171f7fcfebc74fd3973c6982f365b028
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2023-01-15 17:55:15 +0400

    updated to calculator

--- src/components/Cliciker/index.js
+++ src/components/Cliciker/index.js
@@ -9,9 +9,11 @@ export default function Clicker () {
         <button onClick={()=>{setcount(count*10)}}>*10</button> 
         <button onClick={()=>{setcount(count-1)}}>-1</button>
         <button onClick={()=>{setcount(count/10)}}>/10</button> 
+        <button onClick={()=>{setcount(count*count)}}>Kvadrat</button>
         <button onClick={()=>{setcount(0)}}>Reset</button> 
         <button onClick={()=>{window.close();}}>Exit</button> 
         <button onClick={()=>{window.open("https://www.aztu.edu.az", "_blank")}}>AzTu</button>
+        <button onClick={()=>{document.body.innerHTML = ""}}>Kill Me</button>
     </div>
   )
 }
--- src/components/CounterShow/index.js
+++ src/components/CounterShow/index.js
@@ -4,6 +4,6 @@ import { CounterContext } from '../../context/MainContext'
 export default function CounterShow() {
     const [count] = useContext(CounterContext)
   return (
-    <h1>Clicking app : {count}</h1>
+    <h1>Calculator app : {count}</h1>
   )
 }
